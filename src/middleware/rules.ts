54271f1ffe648c42191af7aa28e3f34c49fac98b
@guustavocl <gustavo@gus.sh>
2023-06-22 11:15:45 -0300

    test

--- src/middleware/rules.ts
+++ src/middleware/rules.ts
@@ -3,11 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const rules = () => (req: Request, res: Response, next: NextFunction) => {
-  if (process.env.NODE_MODE === "production") {
-    res.header("Access-Control-Allow-Origin", "https://zoz.bio");
-  } else {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
-  }
+  const cors = {
+    origin: ["https://zoz.bio", "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://localhost:3000"],
+    default: "https://zoz.bio",
+  };
+
+  // if (process.env.NODE_MODE === "production") {
+  //   res.header("Access-Control-Allow-Origin", "https://zoz.bio");
+  // } else {
+  //   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  // }
+  res.header(
+    "Access-Control-Allow-Origin",
+    cors.origin.includes(req.headers.origin || "") ? req.headers.origin : cors.default
+  );
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
 
